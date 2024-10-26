004e85822c8b841f050357091112f9465e1aa387
Ivan Mysovskii <vano641@mail.ru>
2024-10-26 13:59:22 +0300

    добавлен вывод

--- task3_main.java
+++ task3_main.java
@@ -10,16 +10,17 @@ public class task3_main {
 
     public static void main(String[] args) {
         
-        task3_StudentGroup StudentList = new task3_StudentGroup();
+        task3_StudentGroup StudentList = new task3_StudentGroup(); // Печать списка студентов
       //  StudentList.PrintStudentList();
 
         task3_StudentGroupIterator studentInfo = new task3_StudentGroupIterator(StudentList);
         Iterator<task3_Student> student = studentInfo;
 
         while (student.hasNext()) {
-            System.out.println(studentInfo.next());
+            System.out.println(studentInfo.next()); // перебор списка Итератором
         }
-
-        
+     //   studentId: 1, firstName: Иван, lastName: Мысовский, middleName: Андреевич
+     //   studentId: 2, firstName: Кристина, lastName: Мысовская, middleName: Вячеславовна
+     //   studentId: 3, firstName: Артем, lastName: Мысовский, middleName: Иванович
     }
 }
