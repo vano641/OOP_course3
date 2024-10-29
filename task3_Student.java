1ca8840b2e97e11fc142059b12e251734027b392
Ivan Mysovskii <vano641@mail.ru>
2024-10-29 15:49:33 +0300

    Реализация интерфейса Comparable, метод compareTo

--- task3_Student.java
+++ task3_Student.java
@@ -1,4 +1,4 @@
-public class task3_Student {
+public class task3_Student implements Comparable<task3_Student>{
     
     private Integer studentId;
     private String firstName;
@@ -33,6 +33,13 @@ public class task3_Student {
     public String toString() {
         return String.format("studentId: %d, firstName: %s, lastName: %s, middleName: %s",
         studentId, firstName, lastName, middleName);
+    }
+
+    @Override
+    public int compareTo(task3_Student o) {
+// берем у текущего объекта его studentId и вызываем у него метод compareTo
+// у второго объекта также studentId
+        return this.studentId.compareTo(o.studentId);
     }
 
 }
