69ccbeaa10e43713fa7028a228625d60a51d3cb6
Ivan Mysovskii <vano641@mail.ru>
2024-10-29 15:50:40 +0300

    метод сортировки студентов по Id

--- task3_StudentGroupService.java
+++ task3_StudentGroupService.java
@@ -1,5 +1,7 @@
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
-
+import java.util.List;
 public class task3_StudentGroupService {
     
     private task3_StudentGroup studentGroup;
@@ -17,4 +19,12 @@ public class task3_StudentGroupService {
             }
         }
     }
+
+// метод сортировки студентов по Id
+    public List<task3_Student> getSortedStudentList(){
+        List<task3_Student> studentList = new ArrayList<>(studentGroup.getStudentList());        
+        Collections.sort(studentList); 
+        return studentList;
+    }
+
 }
