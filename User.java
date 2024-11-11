56e3489d5b9811ab7bedeea29990d6f472f440e6
Ivan Mysovskii <vano641@mail.ru>
2024-11-11 11:36:24 +0300

    создали Базовый класс User

---
+++ User.java
@@ -0,0 +1,32 @@
+public class User {
+    
+    protected String firstName;
+    protected String lastName;
+    protected String middleName;
+
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMiddleName() {
+        return this.middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+}
